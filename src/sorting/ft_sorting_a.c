b68cbc9bd3141a0d2edfe822011302175830be50
Pietro Falaschi <pitpiter@MacBook-Pro-di-Pietro.local>
2023-03-14 19:48:24 +0100

    devo finire di fare funzione per trovare il nuovo top

--- src/sorting/ft_sorting_a.c
+++ src/sorting/ft_sorting_a.c
@@ -6,7 +6,7 @@
 /*   By: pfalasch <pfalasch@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 23:02:59 by pitpiter          #+#    #+#             */
-/*   Updated: 2023/03/14 16:57:03 by pfalasch         ###   ########.fr       */
+/*   Updated: 2023/03/14 19:46:46 by pfalasch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,8 +69,70 @@ void ft_find_maxmin_a(t_stack *stack)
     per fare questo dobbiamo girare finchè non siamo in ordine.
     per fare questo porto on top sempre
     ricorda; non hai ne i piu grandi ne i piu piccoli nello stack A
-    quindi devi fare in modo di struttura a modo la cosa */
+    quindi devi fare in modo di struttura a modo la cosa 
+    mi trovo il max e il min di A che sono già in ordine ascendente
+    */
 void ft_pushing_to_a(t_stack *stack)
 {
-    
+    int index_top_a;
+    int i;
+    int half_current_a;
+    int half_current_b;
+
+    index_top_a = 0;
+    i = -1;
+    half_current_a = ft_index_half_stack(stack->current_a);
+    half_current_b = ft_index_half_stack(stack->current_b);
+    ft_find_maxmin_a(stack);
+    if (stack->stack_b[0] < stack->stack_a[0])
+    {
+        if (stack->stack_a[0] < stack->stack_a[stack->index_mina])
+            ft_sta_maj_stb_1(stack, half_current_a);
+        else
+            ft_sta_maj_stb_2(stack, half_current_a, index_top_a, i);
+    }
+}
+
+int ft_index_half_stack(int current)
+{   
+    if ((current % 2) != 0)
+        return (current / 2 + 1);
+    else
+        return (current / 2);
+}
+
+ft_sta_maj_stb_1(t_stack *stack, int half_current_a)
+{
+    if (stack->index_mina < half_current_a)
+    {
+        while (stack->index_mina != 0)
+        {
+            ra(stack);
+            ft_find_maxmin_a(stack);
+        }
+    }
+    else
+    {
+        while (stack->index_mina != 0)
+        {
+            rra(stack);
+            ft_find_maxmin_a(stack);
+        }
+    }
+}
+/* questa funzione gestisce la situaizone in cui lo stack_b[0] < stack_a[0]
+    ma non è un nuovo minimo. ciò significa che devo portare on top    */
+ft_sta_maj_std_2(t_stack *stack, int half_current_a, int index_top_a, int i)
+{
+    index_top_a = ft_find_
+}
+/* questa funzione mi serve per trovare l'elemento che devo portare on top 
+affinchè tutto rimanga in ordine una volta che porto l'elemento da B
+dobbiamo trovare  */
+int ft_find_next_top_a (t_stack *stack)
+{
+    int i;
+
+    i = 0;
+    while ()
 }
